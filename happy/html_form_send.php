a8d85eb191ba4fd0871737e8418dc4fe7b4e6507
JoseRDG <joserdg@iMac-de-JoseRDG.local>
2019-02-08 01:52:40 -0500

    mail en php

--- happy/html_form_send.php
+++ happy/html_form_send.php
@@ -2,9 +2,9 @@
 if(isset($_POST['email'])) {
 	
 	// CHANGE THE TWO LINES BELOW
-	$email_to = "you@yourdomain.com";
+	$email_to = "jose.radadg@gmail.com";
 	
-	$email_subject = "website html form submissions";
+	$email_subject = "holanda";
 	
 	
 	function died($error) {
